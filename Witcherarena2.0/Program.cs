1333aac46d9943da4d9818dcc8d9ff6920605f95
patrik <smrzo@ASUS_PATRIK>
2025-10-29 19:45:57 +0100

    Add project files.

---
+++ Witcherarena2.0/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Reflection.Metadata;
+using Witcherarena2._0;
+
+Console.WriteLine(@"
+ /$$      /$$ /$$   /$$               /$$                                  /$$$$$$                                         
+| $$  /$ | $$|__/  | $$              | $$                                 /$$__  $$                                        
+| $$ /$$$| $$ /$$ /$$$$$$    /$$$$$$$| $$$$$$$   /$$$$$$   /$$$$$$       | $$  \ $$  /$$$$$$   /$$$$$$  /$$$$$$$   /$$$$$$ 
+| $$/$$ $$ $$| $$|_  $$_/   /$$_____/| $$__  $$ /$$__  $$ /$$__  $$      | $$$$$$$$ /$$__  $$ /$$__  $$| $$__  $$ |____  $$
+| $$$$_  $$$$| $$  | $$    | $$      | $$  \ $$| $$$$$$$$| $$  \__/      | $$__  $$| $$  \__/| $$$$$$$$| $$  \ $$  /$$$$$$$
+| $$$/ \  $$$| $$  | $$ /$$| $$      | $$  | $$| $$_____/| $$            | $$  | $$| $$      | $$_____/| $$  | $$ /$$__  $$
+| $$/   \  $$| $$  |  $$$$/|  $$$$$$$| $$  | $$|  $$$$$$$| $$            | $$  | $$| $$      |  $$$$$$$| $$  | $$|  $$$$$$$
+|__/     \__/|__/   \___/   \_______/|__/  |__/ \_______/|__/            |__/  |__/|__/       \_______/|__/  |__/ \_______/
+");
+Console.WriteLine("Nacitavam hru......");
+Thread.Sleep(2500);
+Console.Clear();
+
+
+Console.WriteLine("Vitaj v hre Witcher arena 2.0!");
+List<string> menuOpitions = new List<string>() { "Hrat (H) Opis (O)" };
+foreach (string opition in menuOpitions)
+{
+    Console.WriteLine(opition);
+}
+string ChoiceMenu = Console.ReadLine();
+switch (ChoiceMenu.ToUpper())
+{
+    case "H":
+        Console.WriteLine("Zacinas hru.....");
+        Thread.Sleep(1500);
+        break;
+    case "O":
+        Console.Clear();
+        Console.WriteLine("Witcher arena je RPG hra v ktorej budes bojovat v jednotlivych arenach proti roznym priseram");
+        Console.WriteLine("Tuto hru vytvoril Patrik Smrzo");
+        Thread.Sleep(1000);
+        Console.WriteLine("Pre navrat do hlavneho menu stlac ENTER..");
+        Console.ReadLine();
+        Console.Clear();
+        Console.WriteLine("Vitaj v hre Witcher Arena!");
+        foreach (string option in menuOpitions)
+        {
+            Console.WriteLine(option);
+        }
+        ChoiceMenu = Console.ReadLine();
+        break;
+    default:
+        Console.WriteLine("To co si zadal no take tu neni");
+        break;
+}
+
+
+string PlayerName = Console.ReadLine();
+
+
+player Player = new player();
+Player.setName(PlayerName);
+
+Console.WriteLine(PlayerName);
+
---
+++ Witcherarena2.0/player.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace Witcherarena2._0
+{
+    public  class player
+    {
+        private string PlayerName;
+        private const int maxHp = 100;
+        private const int minHp = 0;
+        private int currentHp = maxHp;
+        private int damage = 10;
+
+        internal void setName(string name)
+        {
+            PlayerName = name;
+        }
+    }
+}
